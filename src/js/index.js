66048a95b684c09f4915e56dd485c147d89c94e0
Thomas Coppein <thomas.coppein@esign.eu>
2022-12-07 11:23:14 +0100

    Update: index.js to class;

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,26 @@
 import Cursor from "./Cursor";
 import Magnetic from "./Magnetic";
 
-export default (options) => {
-  // Init cursor
-  const defaultOptions = {
-    magneticElems:
-      typeof options.magneticElms !== "undefined"
-        ? options.magneticElms
-        : document.querySelectorAll("[data-magnetic]"),
-  };
-  window.cursor = new Cursor(options);
+export default class DynamicCursor {
+  constructor(options) {
+    this.options = options;
+    // Init cursor
+    const defaultOptions = {
+      magneticElems:
+        typeof options.magneticElms !== "undefined"
+          ? options.magneticElms
+          : document.querySelectorAll("[data-magnetic]"),
+    };
 
-  [...defaultOptions.magneticElems].forEach((magneticItem) => {
-    window.cursor.magnetic = new Magnetic(magneticItem);
-    Magnetic();
-  });
-};
+    this.initCursor(options);
+
+    [...defaultOptions.magneticElems].forEach((magneticItem) => {
+      window.cursor.magnetic = new Magnetic(magneticItem);
+      Magnetic();
+    });
+  }
+
+  initCursor() {
+    window.cursor = new Cursor(this.options);
+  }
+}
